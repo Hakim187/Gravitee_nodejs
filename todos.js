38254270e74fd3a84b6927fb35eb95b35c822175
Hakim187 <35732014+Hakim187@users.noreply.github.com>
2021-06-23 16:12:23 +0200

    Update todos.js

--- todos.js
+++ todos.js
@@ -1,1 +1,6 @@
-<img class="logo" src="https://res.cloudinary.com/computer-know-how/image/upload/v1588764034/Gravitee" alt="My_Logo">
+<% include header %>
+
+    <div>
+        <h3><img src="https://res.cloudinary.com/computer-know-how/image/upload/v1588764034/Gravitee", alt="Testing Image")</h3>
+    </div>
+ <% include footer %>
