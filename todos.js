3cffed6330799ed6a231a9d4b013880838b15f2c
Hakim187 <35732014+Hakim187@users.noreply.github.com>
2021-06-23 17:14:57 +0200

    Update todos.js

--- todos.js
+++ todos.js
@@ -1,14 +1,6 @@
 module.exports = [
-  {
-    id: 1,
-    task: "Fix Sink"
-  },
-  {
-    id: 2,
-    task: "Buy Groceries"
-  },
-  {
-    id: 3,
-    task: "Wash the dishes"
-  }
-];
+
+
+const imgUrl = "https://repository-images.githubusercontent.com/36487440/95afc980-62e1-11e9-9060-8c0ee67677a1" +icon+ "@2x.png";
+response.write("<img src = "+imgUrl+">");
+response.send();
